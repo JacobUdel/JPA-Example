a37cf4c35389fdcbac8b5c55fcca4c51e7009a56
Jacob Udel <larimer69@gmail.com>
2023-03-31 07:36:09 -0600

    first commit

---
+++ src/main/java/com/example/users/model/User.java
@@ -0,0 +1,42 @@
+package com.example.users.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userId;
+	
+	private String userName;
+	
+	@ManyToMany
+	@JoinTable(name = "user_role",
+	joinColumns = {
+			@JoinColumn(name = "user_id")},
+	inverseJoinColumns = {
+			@JoinColumn(name = "role_id")
+	})
+	@JsonIgnore
+	private Set <Role> role = new HashSet<>();
+	
+
+}
